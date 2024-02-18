ad6259fe74cde670159313a2903e713db31877f5
Denis Orekhov <3008denis@gmail.com>
2024-02-18 14:24:29 +0500

    Make --help option in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,21 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.Scanner;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    String value;
+    public static void main (String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        var input = scanner.next();
+        new CommandLine(new App()).execute(args);
     }
+
+   @Override
+   public void run() {
+   }
 }
