f2bd75580f77ceea416219def12a52ec00dbbd70
Denis Orekhov <3008denis@gmail.com>
2024-02-27 14:47:55 +0500

    Refactoring UtilTest.java

--- app/src/test/java/hexlet/code/UtilsTest.java
+++ app/src/test/java/hexlet/code/UtilsTest.java
@@ -9,12 +9,20 @@ class UtilsTest {
     @Test
     public void right1() throws Exception {
         var actual = getContentOfFile("src/test/resources/file1.json");
-        var expected = "{\n"
-                + "  \"host\": \"hexlet.io\",\n"
-                + "  \"timeout\": 50,\n"
-                + "  \"proxy\": \"123.234.53.22\",\n"
-                + "  \"follow\": false\n"
-                + "}";
-        assertEquals(expected.trim(), actual.trim());
+        var expected = "{\n" +
+                "  \"setting1\": \"Some value\",\n" +
+                "  \"setting2\": 200,\n" +
+                "  \"setting3\": true,\n" +
+                "  \"key1\": \"value1\",\n" +
+                "  \"numbers1\": [1, 2, 3, 4],\n" +
+                "  \"numbers2\": [2, 3, 4, 5],\n" +
+                "  \"id\": 45,\n" +
+                "  \"default\": null,\n" +
+                "  \"checked\": false,\n" +
+                "  \"numbers3\": [3, 4, 5],\n" +
+                "  \"chars1\": [\"a\", \"b\", \"c\"],\n" +
+                "  \"chars2\": [\"d\", \"e\", \"f\"]\n" +
+                "}";
+        assertEquals(expected, actual);
     }
 }
