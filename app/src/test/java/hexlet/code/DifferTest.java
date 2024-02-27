5f7a45df80b6c6e1860d5f23aae4fc150c6a5668
Denis Orekhov <3008denis@gmail.com>
2024-02-27 14:50:24 +0500

    Refactoring DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -28,37 +28,71 @@ class DifferTest {
     public void rightJSON() throws Exception {
         var str1 = "src/test/resources/file1.json";
         var str2 = "src/test/resources/file2.json";
-        var format = "json";
-        var expected = "{\n"
-                + " - follow: false\n"
-                + "   host: hexlet.io\n"
-                + " - proxy: 123.234.53.22\n"
-                + " - timeout: 50\n"
-                + " + timeout: 20\n"
-                + " + verbose: true\n"
-                + "}\n";
+        var format = "stylish";
+        var expected = "{\n" +
+                "    chars1: [a, b, c]\n" +
+                "  - chars2: [d, e, f]\n" +
+                "  + chars2: false\n" +
+                "  - checked: false\n" +
+                "  + checked: true\n" +
+                "  - default: null\n" +
+                "  + default: [value1, value2]\n" +
+                "  - id: 45\n" +
+                "  + id: null\n" +
+                "  - key1: value1\n" +
+                "  + key2: value2\n" +
+                "    numbers1: [1, 2, 3, 4]\n" +
+                "  - numbers2: [2, 3, 4, 5]\n" +
+                "  + numbers2: [22, 33, 44, 55]\n" +
+                "  - numbers3: [3, 4, 5]\n" +
+                "  + numbers4: [4, 5, 6]\n" +
+                "  + obj1: {nestedKey=value, isNested=true}\n" +
+                "  - setting1: Some value\n" +
+                "  + setting1: Another value\n" +
+                "  - setting2: 200\n" +
+                "  + setting2: 300\n" +
+                "  - setting3: true\n" +
+                "  + setting3: none\n" +
+                "}";
 
         generate(str1, str2, format);
 
-        assertEquals(expected, output.toString());
+        assertEquals(expected, output.toString().trim());
     }
 
     @Test
     public void rightYAML() throws Exception {
         var str1 = "src/test/resources/file5.yml";
         var str2 = "src/test/resources/file6.yml";
-        var format = "yml";
-        var expected = "{\n"
-                + " - follow: false\n"
-                + "   host: hexlet.io\n"
-                + " - proxy: 123.234.53.22\n"
-                + " - timeout: 50\n"
-                + " + timeout: 20\n"
-                + " + verbose: true\n"
-                + "}\n";
+        var format = "stylish";
+        var expected = "{\n" +
+                "    chars1: [a, b, c]\n" +
+                "  - chars2: [d, e, f]\n" +
+                "  + chars2: false\n" +
+                "  - checked: false\n" +
+                "  + checked: true\n" +
+                "  - default: null\n" +
+                "  + default: [value1, value2]\n" +
+                "  - id: 45\n" +
+                "  + id: null\n" +
+                "  - key1: value1\n" +
+                "  + key2: value2\n" +
+                "    numbers1: [1, 2, 3, 4]\n" +
+                "  - numbers2: [2, 3, 4, 5]\n" +
+                "  + numbers2: [22, 33, 44, 55]\n" +
+                "  - numbers3: [3, 4, 5]\n" +
+                "  + numbers4: [4, 5, 6]\n" +
+                "  + obj1: {nestedKey=value, isNested=true}\n" +
+                "  - setting1: Some value\n" +
+                "  + setting1: Another value\n" +
+                "  - setting2: 200\n" +
+                "  + setting2: 300\n" +
+                "  - setting3: true\n" +
+                "  + setting3: none\n" +
+                "}";
 
         generate(str1, str2, format);
 
-        assertEquals(expected, output.toString());
+        assertEquals(expected, output.toString().trim());
     }
 }
