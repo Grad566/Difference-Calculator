4c80eb50a4761670a14a00cd0ff4d0c17dabbe7e
Denis Orekhov <3008denis@gmail.com>
2024-02-27 14:47:06 +0500

    Refactoring Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,42 +3,39 @@ package hexlet.code;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import hexlet.code.format.Format;
 
+import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 
 public class Differ {
-    public static void generate(String str, String str2, String format) throws Exception {
-        ObjectMapper mapper;
-
-        if (format.equals("json")) {
-            mapper = new ObjectMapper();
-        } else {
-            mapper = new YAMLMapper();
-        }
-
-        var content1 = Utils.getContentOfFile(str);
-        var content2 = Utils.getContentOfFile(str2);
-        var contentAsMap1 = mapper.readValue(content1, new TypeReference<TreeMap<String, Object>>() { });
-        var contentAsMap2 = mapper.readValue(content2, new TypeReference<TreeMap<String, Object>>() { });
+    public static void generate(String path1, String path2, String format) throws Exception {
+        var contentAsMap1 = Utils.getContentAsTreeMap(path1);
+        var contentAsMap2 = Utils.getContentAsTreeMap(path2);
+        var difference = new ArrayList<String>();
 
         var keys = new TreeSet<String>(contentAsMap1.keySet());
         keys.addAll(contentAsMap2.keySet());
 
-        System.out.println("{");
         for (var key : keys) {
+            if (contentAsMap1.get(key) == null) {
+                contentAsMap1.replace(key, null, "null");
+            }
+
             if (!contentAsMap1.containsKey(key)) {
-                System.out.println(" + " + key + ": " + contentAsMap2.get(key));
+                difference.add("  + " + key + ": " + contentAsMap2.get(key));
             } else if (!contentAsMap2.containsKey(key)) {
-                System.out.println(" - " + key + ": " + contentAsMap1.get(key));
+                difference.add("  - " + key + ": " + contentAsMap1.get(key));
             } else if (contentAsMap1.get(key).equals(contentAsMap2.get(key))) {
-                System.out.println("   " + key + ": " + contentAsMap1.get(key));
+                difference.add("    " + key + ": " + contentAsMap1.get(key));
             } else {
-                System.out.println(" - " + key + ": " + contentAsMap1.get(key));
-                System.out.println(" + " + key + ": " + contentAsMap2.get(key));
+                difference.add("  - " + key + ": " + contentAsMap1.get(key));
+                difference.add("  + " + key + ": " + contentAsMap2.get(key));
             }
         }
-        System.out.println("}");
+
+        Format.printResult(format, difference);
     }
 }
