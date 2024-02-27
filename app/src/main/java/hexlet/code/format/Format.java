e32627ab933d42bd34f56b506fd76b66c00ceacb
Denis Orekhov <3008denis@gmail.com>
2024-02-27 14:49:42 +0500

    Add new methods to Format.java

--- app/src/main/java/hexlet/code/format/Format.java
+++ app/src/main/java/hexlet/code/format/Format.java
@@ -1,2 +1,16 @@
-package hexlet.code.format;public class Format {
+package hexlet.code.format;
+
+import java.util.List;
+
+public class Format {
+    public static void printResult(String format, List<String> difference) {
+        if (format.equals("stylish")) {
+            printAsStylish(difference);
+        }
+    }
+    public static void printAsStylish(List<String> result) {
+        System.out.println("{");
+        result.forEach(System.out::println);
+        System.out.println("}");
+    }
 }
