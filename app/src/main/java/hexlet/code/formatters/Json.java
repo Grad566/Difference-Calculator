a4e69b015ca23cb665a6d7dd4c83567f75c281d4
Denis Orekhov <3008denis@gmail.com>
2024-02-29 13:26:13 +0500

    Refactoring Json.java

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,2 +1,15 @@
-package hexlet.code.formatters;public class Json {
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
+
+public class Json {
+    public static void printAsJson(List<Map<String, Object>> result) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        var resultAsString = objectMapper.writeValueAsString(result);
+        System.out.println(resultAsString);
+    }
 }
