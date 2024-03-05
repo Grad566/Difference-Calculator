475a9e84ff0c5c16d67e8c21cb1f42fba7de099c
Denis Orekhov <3008denis@gmail.com>
2024-03-05 18:27:06 +0500

    Refactoring DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,6 +1,9 @@
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -8,12 +11,12 @@ import static hexlet.code.Differ.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DifferTest {
-    @Test
-    public void rightJSON() throws Exception {
-        var str1 = "src/test/resources/file1.json";
-        var str2 = "src/test/resources/file2.json";
-        var format = "stylish";
-        var expected = "{\n"
+    private static String stylishOutput;
+    private static String plainOutput;
+    private static String jsonOutput;
+    @BeforeAll
+    public static void setExpected() throws IOException {
+        stylishOutput = "{\n"
                 + "    chars1: [a, b, c]\n"
                 + "  - chars2: [d, e, f]\n"
                 + "  + chars2: false\n"
@@ -39,53 +42,7 @@ class DifferTest {
                 + "  + setting3: none\n"
                 + "}";
 
-        var actual = generate(str1, str2, format);
-
-        assertEquals(expected, actual.trim());
-    }
-
-    @Test
-    public void rightYAML() throws Exception {
-        var str1 = "src/test/resources/file5.yml";
-        var str2 = "src/test/resources/file6.yml";
-        var format = "stylish";
-        var expected = "{\n"
-                + "    chars1: [a, b, c]\n"
-                + "  - chars2: [d, e, f]\n"
-                + "  + chars2: false\n"
-                + "  - checked: false\n"
-                + "  + checked: true\n"
-                + "  - default: null\n"
-                + "  + default: [value1, value2]\n"
-                + "  - id: 45\n"
-                + "  + id: null\n"
-                + "  - key1: value1\n"
-                + "  + key2: value2\n"
-                + "    numbers1: [1, 2, 3, 4]\n"
-                + "  - numbers2: [2, 3, 4, 5]\n"
-                + "  + numbers2: [22, 33, 44, 55]\n"
-                + "  - numbers3: [3, 4, 5]\n"
-                + "  + numbers4: [4, 5, 6]\n"
-                + "  + obj1: {nestedKey=value, isNested=true}\n"
-                + "  - setting1: Some value\n"
-                + "  + setting1: Another value\n"
-                + "  - setting2: 200\n"
-                + "  + setting2: 300\n"
-                + "  - setting3: true\n"
-                + "  + setting3: none\n"
-                + "}";
-
-        var actual = generate(str1, str2, format);
-
-        assertEquals(expected, actual.trim());
-    }
-
-    @Test
-    public void rightYAMLWithPlain() throws Exception {
-        var str1 = "src/test/resources/file5.yml";
-        var str2 = "src/test/resources/file6.yml";
-        var format = "plain";
-        var expected = "Property 'chars2' was updated. From [complex value] to false\n"
+        plainOutput = "Property 'chars2' was updated. From [complex value] to false\n"
                 + "Property 'checked' was updated. From false to true\n"
                 + "Property 'default' was updated. From null to [complex value]\n"
                 + "Property 'id' was updated. From 45 to null\n"
@@ -99,6 +56,42 @@ class DifferTest {
                 + "Property 'setting2' was updated. From 200 to 300\n"
                 + "Property 'setting3' was updated. From true to 'none'";
 
+        var path = "src/test/resources/resultForTest.json";
+        var absolutePath = Paths.get(path).toAbsolutePath().normalize();
+        jsonOutput = Files.readString(absolutePath).trim();
+    }
+
+    @Test
+    public void rightJSON() throws Exception {
+        var str1 = "src/test/resources/file1.json";
+        var str2 = "src/test/resources/file2.json";
+        var format = "stylish";
+        var expected = stylishOutput;
+
+        var actual = generate(str1, str2, format);
+
+        assertEquals(expected, actual.trim());
+    }
+
+    @Test
+    public void rightYAML() throws Exception {
+        var str1 = "src/test/resources/file5.yml";
+        var str2 = "src/test/resources/file6.yml";
+        var format = "stylish";
+        var expected = stylishOutput;
+
+        var actual = generate(str1, str2, format);
+
+        assertEquals(expected, actual.trim());
+    }
+
+    @Test
+    public void rightYAMLWithPlain() throws Exception {
+        var str1 = "src/test/resources/file5.yml";
+        var str2 = "src/test/resources/file6.yml";
+        var format = "plain";
+        var expected = plainOutput;
+
         var actual = generate(str1, str2, format);
 
         assertEquals(expected.trim(), actual.trim());
@@ -109,9 +102,7 @@ class DifferTest {
         var str1 = "src/test/resources/file5.yml";
         var str2 = "src/test/resources/file6.yml";
         var format = "json";
-        var path = "src/test/resources/resultForTest.json";
-        var absolutePath = Paths.get(path).toAbsolutePath().normalize();
-        var expected = Files.readString(absolutePath).trim();
+        var expected = jsonOutput;
 
         var actual = generate(str1, str2, format);
 
