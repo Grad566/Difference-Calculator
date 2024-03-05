a5adaaa5409ebdfdfa64e409c8c03b33d8edd737
Denis Orekhov <3008denis@gmail.com>
2024-03-05 18:01:40 +0500

    Refactoring DifferTest.java

--- app/src/test/java/hexlet/code/DifferTest.java
+++ app/src/test/java/hexlet/code/DifferTest.java
@@ -1,10 +1,6 @@
 package hexlet.code;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
