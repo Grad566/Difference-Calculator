9408c3df9e5c6d2045c3ef341f01f7f7a6ef98dc
Denis Orekhov <3008denis@gmail.com>
2024-03-13 14:30:02 +0500

    Rm unused import in Comporator.java

--- app/src/main/java/hexlet/code/Comparator.java
+++ app/src/main/java/hexlet/code/Comparator.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Objects;
 
 public class Comparator {
     public static List<Map<String, Object>> generateDifference(Map<String, Object> map1,
